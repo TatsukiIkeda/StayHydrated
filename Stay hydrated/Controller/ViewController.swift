61c433f0bac92391dc1c2951053d36a4737efeb1
TatsukiIkeda <kisenan411@gmail.com>
2020-07-20 19:18:16 +0900

    起動時userDefaults読み込み実装、各種ボタン機能実装、熱中症指数判定実装

--- Stay hydrated/Controller/ViewController.swift
+++ Stay hydrated/Controller/ViewController.swift
@@ -13,12 +13,13 @@ import Alamofire
 import Foundation
 
 class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSource,UITextFieldDelegate {
-
-
+    
+    
     @IBOutlet weak var targetTextField: UITextField!
     @IBOutlet weak var waterIntakeButton: UIButton!
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var settingGoalButton: UIButton!
+    @IBOutlet weak var WeatherButton: UIButton!
     @IBOutlet weak var fixButton: UIButton!
     @IBOutlet weak var CityPickerView: UIPickerView!
     @IBOutlet weak var intakeSelectPickerView: UIPickerView!
@@ -27,27 +28,28 @@ class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSour
     @IBOutlet weak var goalTargeLabel: UILabel!
     @IBOutlet weak var maximumTemperatureLabel: UILabel!
     @IBOutlet weak var weatherLabel: UILabel!
+    @IBOutlet weak var wbgtLabel: UILabel!
     
     var graphHdata: [Int] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     
     let dataList:[(text: String, ryou: Int)] = [("手動入力", 0),("コップ半分 100ml", 100),("湯飲み1杯 120ml", 120),("コーヒーコップ1杯 150ml", 150),("コップ1杯 200ml", 200),("ペットボトル半分 250ml", 250),("ペットボトル１本 500ml", 500)]
     
-    var inake = 0
-    var tesut = 0
+    var result = 0
+    var inputValue = 0
     var urlData = ""
     var cityData:[String] = ["011000","稚内"]
     var cityList = CityList()
     var date = ""
     var weather = ""
-    var high = ""
+    var high = "0"
     
-
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        getWeathrData(row: 0)
-        print(high)
         
+        
+        getWeathrData(row: 0)
         
         configureObserver()
         CityPickerView.delegate = self
@@ -55,67 +57,63 @@ class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSour
         intakeSelectPickerView.delegate = self
         self.targetTextField.delegate = self
         
-        
+        //ボタンのレイアウト
         waterIntakeButton.layer.cornerRadius = 30.0
         fixButton.layer.cornerRadius = 30.0
         settingGoalButton.layer.cornerRadius = 30.0
         startButton.layer.cornerRadius = 30.0
-        
-        
-        print(high)
-        
-        
-        
-        
-       Chart()
-        // Do any additional setup after loading the view.
+        WeatherButton.layer.cornerRadius = 30.0
+        allRead()
+        Chart()
+
     }
+    
     // Notificationを設定
     func configureObserver() {
-          
-      let notification = NotificationCenter.default
-
-      notification.addObserver(
-        self,
-        selector: #selector(self.keyboardWillShow(notification:)),
-        name: UIResponder.keyboardWillShowNotification,
-        object: nil
-      )
-          
-      notification.addObserver(
-        self,
-        selector: #selector(self.keyboardWillHide(notification:)),
-        name: UIResponder.keyboardWillHideNotification,
-        object: nil
-      )
+        
+        let notification = NotificationCenter.default
+        
+        notification.addObserver(
+            self,
+            selector: #selector(self.keyboardWillShow(notification:)),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
+        
+        notification.addObserver(
+            self,
+            selector: #selector(self.keyboardWillHide(notification:)),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil
+        )
     }
     // Notificationを削除
     func removeObserver() {
-      NotificationCenter.default.removeObserver(self)
+        NotificationCenter.default.removeObserver(self)
     }
     
     
     // キーボードが現れたときにviewをずらす
     @objc func keyboardWillShow(notification: Notification?) {
-      let rect = (notification?.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue
-      let duration: TimeInterval? = notification?.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double
-      UIView.animate(withDuration: duration!) {
-        self.view.transform = CGAffineTransform(translationX: 0, y: -(rect?.size.height)!)
-      }
+        let rect = (notification?.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue
+        let duration: TimeInterval? = notification?.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double
+        UIView.animate(withDuration: duration!) {
+            self.view.transform = CGAffineTransform(translationX: 0, y: -(rect?.size.height)!)
+        }
     }
     // キーボードが消えたときにviewを戻す
     @objc func keyboardWillHide(notification: Notification?) {
-      let duration: TimeInterval? = notification?.userInfo?[UIResponder.keyboardAnimationCurveUserInfoKey] as? Double
-      UIView.animate(withDuration: duration!) {
-        self.view.transform = CGAffineTransform.identity
-      }
+        let duration: TimeInterval? = notification?.userInfo?[UIResponder.keyboardAnimationCurveUserInfoKey] as? Double
+        UIView.animate(withDuration: duration!) {
+            self.view.transform = CGAffineTransform.identity
+        }
     }
     
     //returnが押されたときに呼ばれる.
-       func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-           textField.resignFirstResponder()
-           return true
-       }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
     
     
     
@@ -128,7 +126,7 @@ class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSour
         barChartView.data = data
     }
     
-
+    
     //表示する行の数
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
@@ -142,9 +140,7 @@ class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSour
         }else{
             return cityData.count
         }
-//        return dataList.count
     }
-    
     
     //データを返すメソッド
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
@@ -155,21 +151,22 @@ class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSour
         }else{
             return cityList.list[row].name
         }
-        
     }
+    
     //データ選択時の呼び出しメソッド
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         if pickerView.tag == 1 {
             urlData = cityList.list[row].code
         }else if pickerView.tag == 2 {
-            inake = dataList[row].ryou
+            inputValue = dataList[row].ryou
         }
     }
     
+    //天気予報ボタン
     @IBAction func decisionButton(_ sender: Any) {
         getWeathrData(row: 0)
+        
     }
-    
     
     @IBAction func waterIntakeAction(_ sender: Any) {
         //現在時間取得
@@ -178,13 +175,14 @@ class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSour
         let Hour = calendar.component(.hour, from: date)
         print("\(Hour)")
         
-        graphHdata[Hour] = graphHdata[Hour] + inake
-            
+        graphHdata[Hour] = graphHdata[Hour] + inputValue
+        
         print(graphHdata)
-        let result: Int = graphHdata.reduce(0) { $0 + $1}
-       
+        result = graphHdata.reduce(0) { $0 + $1}
+        
         totalIntakeLabel.text = ("\(result) ml")
         Chart()
+        allSave()
     }
     //グラフにデータを入力処理
     @IBAction func returnAction(_ sender: UIButton) {
@@ -192,82 +190,133 @@ class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSour
         let calendar = Calendar.current
         let Hour = calendar.component(.hour, from: date)
         print("\(Hour)")
-        if graphHdata[Hour] > inake {
-            graphHdata[Hour] = graphHdata[Hour] - inake
-                   print(graphHdata)
-                   let result: Int = graphHdata.reduce(0) { $0 + $1}
-                  
+        if graphHdata[Hour] >= inputValue {
+            graphHdata[Hour] = graphHdata[Hour] - inputValue
+            print(graphHdata)
+            result = graphHdata.reduce(0) { $0 + $1}
+            
             totalIntakeLabel.text = ("\(result) ml")
         }
         Chart()
+        allSave()
     }
     //目標合計水分量を設定
     @IBAction func goalTargeButton(_ sender: Any) {
         goalTargeLabel.text = "\(targetTextField.text!) ml"
+        allSave()
     }
-   
- override func didReceiveMemoryWarning() {
-     super.didReceiveMemoryWarning()
- }
     
-   override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-       
-       self.view.endEditing(true)
-   }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
     
     //Weather Hacksから天気情報を取得
     private func getWeathrData(row: Int) {
         
         var url = "http://weather.livedoor.com/forecast/webservice/json/v1?city=\(urlData)"
         AF.request(url, method: .get, parameters: nil, encoding: JSONEncoding.default).responseJSON {(response) in
-        
-        switch response.result {
             
-            case .success:
+            switch response.result {
                 
-            //jsonを取得
-            let json:JSON = JSON(response.data as Any)
-            
-            //取得したjsonから、必要なデータを取り出す。
-            let date = json["forecasts"][row]["date"].string
-            let weather = json["forecasts"][row]["telop"].string
-            let high = json["forecasts"][row]["temperature"]["max"]["celsius"].string
-            
-            
-            if date != nil {
-                self.date = String(date!.suffix(5))  //2025-04-10 -> 04-10
-            } else {
-                self.date = "No Data"
-            }
-            
-            if weather != nil {
-                self.weather = weather!
-            } else {
-                self.weather = "No Data"
-            }
-
-            if high != nil {
-                self.high = "\(high!)°"
-            } else {
-                self.high = "No Data"
-            }
-            
-            self.setWeatherData(row: 0)
-            
-        case .failure(let error):
-        
-        print(error)
+            case .success:
+                //jsonを取得
+                let json:JSON = JSON(response.data as Any)
+                
+                //取得したjsonから、必要なデータを取り出す。
+                let date = json["forecasts"][row]["date"].string
+                let weather = json["forecasts"][row]["telop"].string
+                let high = json["forecasts"][row]["temperature"]["max"]["celsius"].string
+                
+                if date != nil {
+                    self.date = String(date!.suffix(5))  //2025-04-10 -> 04-10
+                } else {
+                    self.date = "No Data"
+                }
+                
+                if weather != nil {
+                    self.weather = weather!
+                } else {
+                    self.weather = "No Data"
+                }
+                
+                if high != nil {
+                    self.high = "\(high!)"
+                } else {
+                    self.high = "0"
+                }
+//
+                self.setWeatherData(row: 0)
+//                self.allSave()
+            case .failure(let error):
+                print(error)
             }
         }
     }
+    
     //ラベルに反映
     private func setWeatherData(row: Int) {
-            weatherLabel.text = weather
-            maximumTemperatureLabel.text =  high
-       
-    }
+        weatherLabel.text = weather
+        //最高気温表示
+        maximumTemperatureLabel.text =  "\(high)°"
         
+        var highInt = 0
+        highInt = Int(high)!
+        //WBGT指数判定・表示
+        if highInt > 31 {
+            wbgtLabel.text = "危険"
+        }else if highInt > 28 {
+            wbgtLabel.text = "厳重警戒"
+        }else if highInt > 25 {
+            wbgtLabel.text = "警戒"
+        }else if highInt > 21 {
+            wbgtLabel.text = "注意"
+        }else{
+            wbgtLabel.text = "ぼぼ安全"
+        }
+        if "\(high)" == "0" {
+            maximumTemperatureLabel.text = "この時間帯では"
+            wbgtLabel.text = "取得できません"
+        }
+    }
+    func wbgtJudgment() {
+    }
+    //データを全て保存
+    func allSave() {
+        
+        let userDefaults = UserDefaults.standard
+        userDefaults.set(graphHdata, forKey: "graphHdata")
+        userDefaults.set(result , forKey: "totalIntakeLabel")
+        userDefaults.set(goalTargeLabel.text , forKey: "goalTargeLabel")
+        userDefaults.synchronize()
+    }
     
+    //全てのデータを読み込み
+    func allRead() {
+        //初期値を決める
+        let userDefaults = UserDefaults.standard
+        userDefaults.register(defaults: ["graphHdata": graphHdata,"totalIntakeLabel": "","goalTargeLabel": ""])
+    
+        graphHdata = UserDefaults.standard.object(forKey: "graphHdata") as! [Int]
+        totalIntakeLabel.text = UserDefaults.standard.string(forKey: "totalIntakeLabel")
+        goalTargeLabel.text = UserDefaults.standard.string(forKey: "goalTargeLabel")
+    }
+    //初期化
+    @IBAction func initializationButton(_ sender: Any) {
+        let userDefaults = UserDefaults.standard
+        if let domain = Bundle.main.bundleIdentifier {
+                    userDefaults.removePersistentDomain(forName: domain)
+                }
+        userDefaults.synchronize()
+        
+        graphHdata = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        totalIntakeLabel.text = "一日合計摂取量"
+        goalTargeLabel.text = "目標合計摂取量"
+        Chart()
+    }
     
     
 }
